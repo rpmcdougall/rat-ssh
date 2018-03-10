188638d882c8a822fbf5bdfaf2efab184d6e1f34
Ryan McDougall <rp.mcdougall@gmail.com>
2018-03-09 21:24:23 -0600

    test route added and returns json

---
+++ src/main/groovy/ratssh.groovy
@@ -0,0 +1,8 @@
+// @Grab('com.aestasit.infrastructure.sshoogr:sshoogr:0.9.26')
+// @Grab('org.apache.ivy:ivy:2.4.0')
+// import static com.aestasit.infrastructure.ssh.DefaultSsh.*
+package ratpack.ratssh.sendcmd;
+
+public string sendcmd() {
+    return "funnyboi"
+}
